7011a6ac6c10833ace6fa8a6e4bb52ab47c2d1b1
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-02-25 00:29:28 -0300

    Remove unnecessary CSS

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -5,11 +5,6 @@ const Container = styled.div`
     border-radius: 5px;
     border: 5px solid #e8e400;
     margin: 0;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
 
     svg {
         font-size: 20px;
