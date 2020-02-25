fb34f4f3c4bebedaf99bf847005c7668a0adad7c
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-02-25 10:53:18 -0300

    Implements cache with localstorage

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,81 +1,110 @@
 import React, { Component } from 'react';
 import { FaSpinner } from 'react-icons/fa';
-import api from '../../services/api'
+
+import api from '../../services/api';
 import Container from '../Container/styles';
 import { Info, SubmitButton } from './styles';
 
 export default class Main extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             loading: false,
-            planet : {}
-        }
+            id: 0,
+            planet: {},
+        };
     }
 
-    generateRandomId(max, min){
-        return Math.round(Math.random() * (max - min) + min)
+    async componentDidMount() {
+        const randomPlanetId = this.generateRandomId(60, 1);
+        const planetsInCache = localStorage.getItem(randomPlanetId);
+
+        if (planetsInCache) {
+            return this.setState({
+                planet: JSON.parse(planetsInCache),
+                id: randomPlanetId,
+            });
+        }
+
+        const response = await api.get(`planets/${randomPlanetId}/`);
+        localStorage.setItem(randomPlanetId, JSON.stringify(response.data));
+
+        return this.setState({ planet: response.data, id: randomPlanetId });
     }
 
     componentDidUpdate(_, prevState) {
-        const { planet } = this.state;
+        const { planet, id } = this.state;
         if (prevState !== planet) {
-            localStorage.setItem('planet', JSON.stringify(planet));
+            localStorage.setItem(id, JSON.stringify(planet));
         }
-    }
-
-    async componentDidMount(){
-        const planetsInCache = localStorage.getItem("planet")
-
-        if(planetsInCache){
-            return this.setState({ planet:  JSON.parse(planetsInCache)})
-        }
-
-        const response = await api.get(`planets/${this.generateRandomId(60, 1)}/`)
-        this.setState({ planet: response.data })
     }
 
     handleClick = async e => {
-        e.preventDefault()
+        e.preventDefault();
 
-        this.setState({ loading: true })
-        let randomNumber = this.generateRandomId(60, 1)
-        const response = await api.get(`planets/${randomNumber}/`)
+        this.setState({ loading: true });
+        const randomPlanetId = this.generateRandomId(61, 1);
+        const planetInCache = localStorage.getItem(randomPlanetId);
 
-        this.setState({
+        if (planetInCache) {
+            return this.setState({
+                planet: JSON.parse(planetInCache),
+                id: randomPlanetId,
+                loading: false,
+            });
+        }
+
+        const response = await api.get(`planets/${randomPlanetId}/`);
+        return this.setState({
             planet: response.data,
-            loading: false
-        })
-    }
+            id: randomPlanetId,
+            loading: false,
+        });
+    };
+
+    generateRandomId = (max, min) =>
+        Math.round(Math.random() * (max - min) + min);
 
     render() {
-        let { planet, loading } = this.state
+        const { planet, loading } = this.state;
 
         return (
-             planet && planet.films !== undefined && <Container>
-                <Info>
-                    <section className="planet-name-container">
-                        <h1>Planet {planet.name}</h1>
-                    </section>
+            planet &&
+            planet.films !== undefined && (
+                <Container>
+                    <Info>
+                        <section className="planet-name-container">
+                            <h1>Planet {planet.name}</h1>
+                        </section>
 
-                    <section className="planet-information-container">
-                        <p>Population: <span>{planet.population}</span></p>
-                        <p>Climate: <span>{planet.climate}</span></p>
-                        <p>Terrain: <span>{planet.terrain}</span></p>
-                        <p>Gravity: <span>{planet.gravity}</span></p>
-                        <p>Diameter: <span>{planet.diameter}</span></p>
-                        <p>Featured in {planet.films.length} films</p>
-                    </section>
-
-                </Info>
-                <SubmitButton onClick={this.handleClick} loading={loading}>
-                    { loading ? (
-                        <FaSpinner color="black" size={13} />
-                    ) : (
-                        "Next"
-                    )}
-                </SubmitButton>
-            </Container>
-        )
+                        <section className="planet-information-container">
+                            <p>
+                                Population: <span>{planet.population}</span>
+                            </p>
+                            <p>
+                                Climate: <span>{planet.climate}</span>
+                            </p>
+                            <p>
+                                Terrain: <span>{planet.terrain}</span>
+                            </p>
+                            <p>
+                                Gravity: <span>{planet.gravity}</span>
+                            </p>
+                            <p>
+                                Diameter: <span>{planet.diameter}</span>
+                            </p>
+                            <p>Featured in {planet.films.length} films</p>
+                        </section>
+                    </Info>
+                    <SubmitButton onClick={this.handleClick} loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="black" size={13} />
+                        ) : (
+                            'Next'
+                        )}
+                    </SubmitButton>
+                </Container>
+            )
+        );
     }
 }
