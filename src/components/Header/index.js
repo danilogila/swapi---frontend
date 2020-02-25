de33f699bb1540c1ecc4edfd92c1bbfbbbd0d1cb
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-02-25 00:28:39 -0300

    Add Header with Star Wars logo

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../../assets/logo.png'
+import './styles.css';
+
+const Header = () => {
+    return (
+        <header>
+            <img src={Logo} />
+        </header>
+    )
+};
+
+export default Header;
