9ffa8b4fdeae31b2b811455e0f1fa6984b2ff11a
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-02-25 00:29:05 -0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from './components/Main'
 import GlobalStyles from './styles/global'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      <GlobalStyles />
+        <Header/>
+        <Main />
+        <GlobalStyles />
     </div>
   );
 }
