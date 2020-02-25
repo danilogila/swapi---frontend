cc9d45ed63357e6619d5bc5b75c30ab9d7d4faee
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-02-25 10:52:49 -0300

    Apply style guide

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Main from './components/Main'
-import GlobalStyles from './styles/global'
-import Header from './components/Header'
+import Main from './components/Main';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Main />
-        <GlobalStyles />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Main />
+            <GlobalStyles />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.png';
 import './styles.css';
 
 const Header = () => {
     return (
         <header>
-            <img src={Logo} />
+            <img src={Logo} alt="Star Wars logo" />
         </header>
-    )
+    );
 };
 
 export default Header;
